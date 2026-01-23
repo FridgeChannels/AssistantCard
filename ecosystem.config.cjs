16f708cc042c239c1bf932c4128b9b51dfceccad
Mark-Fridge-Channels <mark@fridgechannels.com>
2026-01-23 13:06:07 +0800

    更新配置

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,15 +2,14 @@ module.exports = {
   apps: [
     {
       name: 'assistant-card',
-      script: 'npm',
-      args: 'start',
+      script: './node_modules/.bin/serve',
+      args: 'dist -l 4173 -s',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
       env: {
         NODE_ENV: 'production',
-        DANGEROUSLY_DISABLE_HOST_CHECK: 'true',
       },
     },
   ],
