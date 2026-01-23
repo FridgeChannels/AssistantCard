5317d49cbf5aff1ca24ec0732eb937d6ca4c6ecb
Mark-Fridge-Channels <mark@fridgechannels.com>
2026-01-23 13:12:00 +0800

    更新配置

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'assistant-card',
       script: './node_modules/.bin/serve',
-      args: 'dist -l 4173 -s',
+      args: 'dist -p 4173 -s',
       instances: 1,
       autorestart: true,
       watch: false,
